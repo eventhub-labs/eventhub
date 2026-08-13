43543f567f635f245a5279b7520efc391995998d
Stanislav <mohoidstanoslav@gmail.com>
2026-08-13 22:54:22 +0200

    Added login/home page ui. Added themes functionality with toggle theme component

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-background flex flex-col gap-3 justify-center items-center">
+      <h1 className="text-4xl font-bold text-foreground">Dashboard</h1>
+    </div>
+  );
+}
