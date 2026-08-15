b46b3e8edf90fd88c9598534fd208ac16013a4f1
Yehor Prykhodko <yehor.prykhodko8@etu.univ-lorraine.fr>
2026-08-15 22:56:21 +0300

    auth exceptions

---
+++ backend/src/main/java/com/eventhub/eventapp/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.eventhub.eventapp.common.exception;
+
+import com.eventhub.eventapp.auth.exception.UserAlreadyExistsException;
+import com.eventhub.eventapp.common.exception.dto.ErrorResponseDTO;
+import com.eventhub.eventapp.common.exception.dto.ValidationErrorResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDTO> handleUserAlreadyExists(UserAlreadyExistsException ex){
+        return new ResponseEntity<>(new ErrorResponseDTO(ex.getMessage()), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponseDTO> handleMethodArgumentNotValid(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+
+        for( FieldError fieldError : ex.getBindingResult().getFieldErrors() )
+           errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+
+        return new ResponseEntity<>(new ValidationErrorResponseDTO(errors), HttpStatus.BAD_REQUEST);
+    }
+
+}
