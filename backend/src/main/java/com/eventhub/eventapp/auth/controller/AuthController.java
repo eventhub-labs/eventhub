09804e22db311bcfe54d59fc26844e2ff34e33d8
Yehor Prykhodko <yehor.prykhodko8@etu.univ-lorraine.fr>
2026-08-18 00:48:27 +0300

    auth controller login/rehister/refresh

--- backend/src/main/java/com/eventhub/eventapp/auth/controller/AuthController.java
+++ backend/src/main/java/com/eventhub/eventapp/auth/controller/AuthController.java
@@ -1,19 +1,24 @@
 package com.eventhub.eventapp.auth.controller;
 
-import com.eventhub.eventapp.auth.dto.RegisterRequestDTO;
-import com.eventhub.eventapp.auth.dto.RegisterResponseDTO;
+import com.eventhub.eventapp.auth.dto.*;
+import com.eventhub.eventapp.auth.exception.InvalidRefreshTokenException;
 import com.eventhub.eventapp.auth.service.AuthService;
+import com.eventhub.eventapp.auth.service.model.LoginResult;
+import com.eventhub.eventapp.auth.service.model.RegisterResult;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+    @Value("${app.jwt.refresh-ttl}")
+    private int refreshTTL;
 
     private final AuthService authService;
 
@@ -23,6 +28,49 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResponseEntity<RegisterResponseDTO> register(@Valid @RequestBody RegisterRequestDTO request){
-        return new ResponseEntity<>(authService.register(request), HttpStatus.CREATED);
+        RegisterResult registerResult = authService.register(request);
+
+        ResponseCookie refreshCookie = ResponseCookie.from(
+                        "refreshToken",
+                        registerResult.refreshToken()
+                )
+                .httpOnly(true)
+                .secure(false)
+                .sameSite("Lax")
+                .path("/auth/refresh")
+                .maxAge(refreshTTL)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CREATED).header(HttpHeaders.SET_COOKIE, refreshCookie.toString()).body(registerResult.response());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO request){
+
+        LoginResult loginResult = authService.login(request);
+
+        ResponseCookie refreshCookie = ResponseCookie.from(
+                        "refreshToken",
+                        loginResult.refreshToken()
+                )
+                .httpOnly(true)
+                .secure(false)
+                .sameSite("Lax")
+                .path("/auth/refresh")
+                .maxAge(refreshTTL)
+                .build();
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, refreshCookie.toString()).body(loginResult.response());
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<RefreshResponseDTO> refresh( @CookieValue(name = "refreshToken", required = false) String refreshToken){
+        if(refreshToken==null){
+            throw new InvalidRefreshTokenException("Invalid refresh token");
+        }
+
+       return ResponseEntity.ok(this.authService.refresh(refreshToken));
+
+
     }
 }
