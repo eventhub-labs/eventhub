54b53734f2bd0f7e484d410f522fe37db650f890
Yehor Prykhodko <yehor.prykhodko8@etu.univ-lorraine.fr>
2026-08-18 00:44:15 +0300

    auth cfg

--- backend/src/main/java/com/eventhub/eventapp/config/SecurityConfig.java
+++ backend/src/main/java/com/eventhub/eventapp/config/SecurityConfig.java
@@ -1,27 +1,40 @@
 package com.eventhub.eventapp.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtDecoder accessJwtDecoder;
+
+    SecurityConfig(@Qualifier("accessJwtDecoder") JwtDecoder accessJwtDecoder){
+        this.accessJwtDecoder = accessJwtDecoder;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http.cors(Customizer.withDefaults());
         http.csrf(AbstractHttpConfigurer::disable);
+
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/register").permitAll()
+                .requestMatchers("/auth/register", "/auth/login", "/auth/refresh").permitAll()
                 .anyRequest().authenticated()
         );
+
+        http.oauth2ResourceServer(oauth -> oauth.jwt(jwt -> jwt.decoder(accessJwtDecoder)));
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
