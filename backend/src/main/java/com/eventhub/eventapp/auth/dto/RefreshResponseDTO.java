23a1c06d2bea088d000ace1333e7e750f3b195ed
Yehor Prykhodko <yehor.prykhodko8@etu.univ-lorraine.fr>
2026-08-18 22:44:57 +0300

    fix return user data on token refresh

--- backend/src/main/java/com/eventhub/eventapp/auth/dto/RefreshResponseDTO.java
+++ backend/src/main/java/com/eventhub/eventapp/auth/dto/RefreshResponseDTO.java
@@ -1,5 +1,10 @@
 package com.eventhub.eventapp.auth.dto;
 
 public record RefreshResponseDTO(
+        String email,
+        String username,
+        String name,
+        String surname,
+        String imgUrl,
         String accessToken
 ) {}
