9c35c4d86017f133d2baebc2799d3be50bd89102
Stanislav <mohoidstanoslav@gmail.com>
2026-08-22 21:28:04 +0200

    Fixed using a tag instead of Link

--- frontend/app/dashboard/_components/user-menu-item.tsx
+++ frontend/app/dashboard/_components/user-menu-item.tsx
@@ -10,6 +10,7 @@ import logout from "@/features/auth/logout";
 import { useUser } from "@/store/user";
 import { LogOut } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import { toast } from "sonner";
 
@@ -44,7 +45,10 @@ export default function UserMenuItem() {
 
   return (
     <SidebarMenuItem>
-      <SidebarMenuButton render={<a href="/dashboard/user" />} className="p-1">
+      <SidebarMenuButton
+        render={<Link href="/dashboard/user" />}
+        className="p-1"
+      >
         <div className="relative aspect-square h-full">
           <Image
             className="w-12"
--- frontend/features/auth/restore-user-hydrator.tsx
+++ frontend/features/auth/restore-user-hydrator.tsx
@@ -23,6 +23,7 @@ export default function RestoreUserHydrator() {
           toast.warning(err);
         });
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
