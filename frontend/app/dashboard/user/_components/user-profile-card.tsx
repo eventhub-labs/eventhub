4b08b3abfd6d39e6627df2cbdad473854c82a18b
Stanislav <mohoidstanoslav@gmail.com>
2026-09-05 20:20:28 +0200

    Added file uploading ui

--- frontend/app/dashboard/user/_components/user-profile-card.tsx
+++ frontend/app/dashboard/user/_components/user-profile-card.tsx
@@ -1,8 +1,26 @@
-import { Button } from "@/components/ui/button";
+import {
+  Attachment,
+  AttachmentAction,
+  AttachmentActions,
+  AttachmentContent,
+  AttachmentDescription,
+  AttachmentMedia,
+  AttachmentTitle,
+} from "@/components/ui/attachment";
+import Dropzone from "react-dropzone";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Skeleton } from "@/components/ui/skeleton";
-import { ImageUp } from "lucide-react";
+import { FileUp, ImageUp, XIcon } from "lucide-react";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 const UserProfileField = ({
   fieldName,
@@ -74,13 +92,59 @@ export default function UserProfileCard({
             width={100}
             height={100}
           />
-          <Button
-            variant="default"
-            size="icon-xs"
-            className="absolute right-0 bottom-0 rounded-full"
-          >
-            <ImageUp />
-          </Button>
+          <Dialog>
+            <DialogTrigger className="bg-muted hover:bg-muted-foreground absolute right-0 bottom-0 rounded-full p-1 transition hover:scale-110">
+              <div className="rounded-full">
+                <ImageUp size={20} />
+              </div>
+            </DialogTrigger>
+
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Image Uploading</DialogTitle>
+                <DialogDescription>Choose file to upload</DialogDescription>
+              </DialogHeader>
+              <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+                {({ getRootProps, getInputProps }) => (
+                  <section className="border-2 border-dashed">
+                    <div
+                      {...getRootProps()}
+                      className="flex flex-col items-center justify-center p-10"
+                    >
+                      <input {...getInputProps()} />
+                      <FileUp size={30} />
+                      <p className="text-muted-foreground mt-4 px-8 text-center">
+                        Drag &apos;n&apos; drop some file here, or click to
+                        select file
+                      </p>
+                    </div>
+                  </section>
+                )}
+              </Dropzone>
+
+              <Attachment state="idle">
+                <AttachmentMedia variant="image">
+                  <Image
+                    src="/img/no-user.png"
+                    alt="uploaded-photo"
+                    width={100}
+                    height={100}
+                  />
+                </AttachmentMedia>
+                <AttachmentContent>
+                  <AttachmentTitle>sales-dashboard.pdf</AttachmentTitle>
+                  <AttachmentDescription>PDF · 2.4 MB</AttachmentDescription>
+                </AttachmentContent>
+                <AttachmentActions>
+                  <AttachmentAction aria-label="Remove sales-dashboard.pdf">
+                    <XIcon />
+                  </AttachmentAction>
+                </AttachmentActions>
+              </Attachment>
+
+              <Button variant="outline">Upload</Button>
+            </DialogContent>
+          </Dialog>
         </div>
         <h2 className="mt-3 text-2xl font-bold">
           {name} {surname}
